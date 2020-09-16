155cfe71421ffe1f29336554ae60214f9d207239
YJS <dbswhdtjr0513@naver.com>
2020-09-16 14:58:14 +0900

    no message

--- Lab2/array.c
+++ Lab2/array.c
@@ -5,10 +5,8 @@ int get_index_of(const int numbers[], const size_t element_count, const int num)
 {
     size_t i;
     int index = 0;
-    for (i = 0; i < element_count; i++)
-    {
-        if (numbers[i] == num) 
-        {
+    for(i = 0; i < element_count; i++){
+        if(numbers[i] == num) {
             index = i;
             return index;
         }
@@ -20,10 +18,8 @@ int get_last_index_of(const int numbers[], const size_t element_count, const int
 {
     int i;
     int index = 0;
-    for (i = (int)element_count - 1; i >= 0; i--) 
-    {
-        if (numbers[i] == num) 
-        {
+    for(i = (int)element_count - 1; i >= 0; i--) {
+        if(numbers[i] == num) {
             index = i;
             return index;
         }
@@ -35,15 +31,12 @@ int get_max_index(const int numbers[], const size_t element_count)
 {
     size_t i;
     int max_index;
-    int max = INT_MIN+1;
-    if (element_count == 0) 
-    {
+    int max = INT_MIN + 1;
+    if(element_count == 0) {
         return -1;
     }
-    for (i = 0; i < element_count; i++) 
-    {
-        if (numbers[i] > max) 
-        {
+    for(i = 0; i < element_count; i++) {
+        if(numbers[i] > max) {
             max = numbers[i];
             max_index = i;
         }
@@ -56,22 +49,17 @@ int get_min_index(const int numbers[], const size_t element_count)
     size_t i;
     size_t count = 0;
     int min_index;
-    int min = INT_MAX-1;
-    for (i = 0; i < element_count; i++) 
-    {
-        if (numbers[i] == INT_MAX || numbers[i] == INT_MIN) 
-        {
+    int min = INT_MAX - 1;
+    for(i = 0; i < element_count; i++) {
+        if(numbers[i] == INT_MAX || numbers[i] == INT_MIN) {
             count += 1;
         }
     }
-    if (count == element_count) 
-    {
+    if(count == element_count) {
         return -1;
     }
-    for (i = 0; i < element_count; i++) 
-    {
-        if (numbers[i] < min)
-        {
+    for(i = 0; i < element_count; i++) {
+        if(numbers[i] < min){
             min = numbers[i];
             min_index = i;
         }
@@ -83,34 +71,26 @@ int is_all_positive(const int numbers[], const size_t element_count)
 {
     size_t i;
     size_t count = 0;
-    for (i = 0; i < element_count; i++)
-    {
-        if (numbers[i] == INT_MIN || numbers[i] == INT_MAX)
-        {
+    for(i = 0; i < element_count; i++){
+        if(numbers[i] == INT_MIN || numbers[i] == INT_MAX){
             count++;
         }
     }
-    if (count == element_count)
-    {
+    if(count == element_count){
         return FALSE;
     }
-    else
-    {
+    else{
         count = 0;
     }
-    for (i = 0; i < element_count; i++) 
-    {
-        if (numbers[i] >= 0)
-        {
+    for(i = 0; i < element_count; i++) {
+        if(numbers[i] >= 0){
             count++;
         }
-        else if (numbers[i] < 0)
-        {
+        else if(numbers[i] < 0){
             return FALSE;
         }
     }
-    if (count == element_count)
-    {
+    if(count == element_count){
         return TRUE;
     }
 
@@ -120,14 +100,11 @@ int is_all_positive(const int numbers[], const size_t element_count)
 int has_even(const int numbers[], const size_t element_count) 
 {
     size_t i;
-    for (i = 0; i < element_count; i++) 
-    {
-        if (numbers[i] % 2 == 0) 
-        {
+    for(i = 0; i < element_count; i++) {
+        if(numbers[i] % 2 == 0) {
             return TRUE;
         }
-        else 
-        {
+        else {
             continue;
         }
     }
@@ -138,17 +115,14 @@ int insert(int numbers[], const size_t element_count, const int num, const size_
 {
     int i;
 
-    if(pos >= 0 && pos <= element_count)
-    {
-        for (i = (int)element_count - 1; i >= (int)pos; i--)
-        {
+    if(pos >= 0 && pos <= element_count){
+        for (i = (int)element_count - 1; i >= (int)pos; i--){
             numbers[i + 1] = numbers[i];
         }
         numbers[pos] = num;
         return TRUE;
     }
-    else
-    {
+    else{
         return FALSE;
     }
 
@@ -159,23 +133,18 @@ int remove_at(int numbers[], const size_t element_count, const size_t index)
 {
     size_t i;
     size_t count = 0;
-    if ((int)index > (int)element_count - 1 || index < 0) 
-    {
+    if ((int)index > (int)element_count - 1 || index < 0) {
         return FALSE;
     }
-    for (i = 0; i < element_count; i++) 
-    {
-        if (numbers[i] == INT_MIN || numbers[i] == INT_MAX) 
-       {
+    for (i = 0; i < element_count; i++) {
+        if (numbers[i] == INT_MIN || numbers[i] == INT_MAX) {
             count += 1;
        }
     }
-    if (count == element_count) 
-    {
+    if (count == element_count) {
         return FALSE;
     }
-    for (i = index + 1; i < element_count; i++) 
-    {
+    for (i = index + 1; i < element_count; i++) {
         numbers[i - 1] = numbers[i];
     }
     numbers[element_count-1] = INT_MIN;
