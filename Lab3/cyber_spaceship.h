1ad0d18d4e57242901460e5b84b15b6f7d8d68d5
YJS <dbswhdtjr0513@naver.com>
2020-09-24 20:12:44 +0900

    no message

--- Lab3/cyber_spaceship.h
+++ Lab3/cyber_spaceship.h
@@ -5,5 +5,4 @@ const char* get_longest_safe_zone_or_null(const char* const cab_start_location, 
 
 int get_travel_time(const char* const cab_start_location, const size_t cab_length, const char* const cluster_start_locations[], const size_t cluster_lengths[], const size_t cluster_count);
 
-const char* safe_location(const char* const cab_start_location, int i);
 #endif /* CYBER_SPACESHIP */
