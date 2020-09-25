b301cbb3d26bf01d2847490ce21490f60d128151
YJS <dbswhdtjr0513@naver.com>
2020-09-25 22:40:16 +0900

    no message

--- Lab3/cyber_spaceship.c
+++ Lab3/cyber_spaceship.c
@@ -1,7 +1,6 @@
 #include <limits.h>
 #include "cyber_spaceship.h"
 
-#define NULL ((void *)0)
 const char* get_longest_safe_zone_or_null(const char* const cab_start_location, const size_t cab_length, const char* const cluster_start_locations[], const size_t cluster_lengths[], const size_t cluster_count, size_t* out_longest_safe_area_length)
 {
     if (cab_length == 0) { /* cab의 길이가 0일 때*/
